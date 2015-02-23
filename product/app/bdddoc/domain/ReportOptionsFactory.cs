4e223bee19b1a6f57914647654c47591c2f7a42a
Rob Reynolds <ferventcoder@gmail.com>
2015-02-23 10:42:08 -0600

    Set markdown and custom header options

--- product/app/bdddoc/domain/ReportOptionsFactory.cs
+++ product/app/bdddoc/domain/ReportOptionsFactory.cs
@@ -17,17 +17,41 @@ namespace bdddoc.domain
 
         public IReportOptions create_from(string[] args)
         {
+            if (args.Length == 0) return new MissingReportOptions();
+
             try
             {
-                return new ReportOptions
-                           {
-                               assembly_to_scan = assembly_resolver.find_using(args[0]),
-                               observation_specification = new ObservationSpecification(args[1]),
-                               output_filename = args[2]
-                           };
+                Console.WriteLine("{0}", string.Join(" ", args));
+
+                var options = new ReportOptions
+                    {
+                        assembly_to_scan = assembly_resolver.find_using(args[0]),
+                        observation_specification = new ObservationSpecification(args[1]),
+                        output_filename = args[2],
+                    };
+
+                foreach (var arg in args)
+                {
+                    if (arg.Contains("markdown"))
+                    {
+                        options.report_type = BddReportType.Markdown;
+                    }
+                }   
+                
+                foreach (var arg in args)
+                {
+                    if (arg.Contains("custom_header"))
+                    {
+                        options.custom_header = arg.Replace("custom_header", string.Empty).Trim();
+                        if (options.custom_header.StartsWith("=")) options.custom_header = options.custom_header.Remove(0, 1);
+                    }
+                }
+
+                return options;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                Console.WriteLine("Error occurred:{0} {1}", Environment.NewLine, ex);
                 return new MissingReportOptions();
             }
         }
