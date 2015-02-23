a0d6f92f6010aaf1a5a28957d100d7beba186248
Rob Reynolds <ferventcoder@gmail.com>
2015-02-23 10:31:29 -0600

    console report input / pause when done (debug only)

--- product/app/bdddoc/console/BootStrap.cs
+++ product/app/bdddoc/console/BootStrap.cs
@@ -1,12 +1,20 @@
-using bdddoc.tasks;
-
 namespace bdddoc.console
 {
+    using System;
+    using tasks;
+
     public class BootStrap
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Received {0}", Environment.CommandLine);
+
             new ReportTasks().run_report_using(args);
+
+#if DEBUG
+            Console.WriteLine("Press Enter to Continue...");
+            Console.ReadLine();
+#endif
         }
     }
 }
